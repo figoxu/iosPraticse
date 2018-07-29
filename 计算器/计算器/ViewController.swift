39903595f2c4cde5c5532e99ad0325854006937a
figoxu <xujh945@qq.com>
2018-07-29 18:09:33 +0800

    初始化界面

--- 计算器/计算器/ViewController.swift
+++ 计算器/计算器/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelResult: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.yellow
+        let label = UILabel(frame:CGRect(x:10,y:370,width:300,height:50))
+        label.text = "你好"
+        label.textColor = UIColor.red
+        self.view.addSubview(label)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
