9c722157270ae9defd942c0acb0ac52b4dfb1a53
figoxu <xujh945@qq.com>
2018-07-29 21:43:20 +0800

    绑定按钮，并处理输入事件

--- 计算器/计算器/ViewController.swift
+++ 计算器/计算器/ViewController.swift
@@ -11,14 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var labelResult: UILabel!
+    
+    var firstOperand:Double = 0.0
+    var secondOperand:Double = 0.0
+    var decimalPointFlag:Bool = false
+    var isSecond:Bool = false
+    var operatorFlag:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.yellow
-        let label = UILabel(frame:CGRect(x:10,y:370,width:300,height:50))
-        label.text = "你好"
-        label.textColor = UIColor.red
-        self.view.addSubview(label)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -27,6 +28,86 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonTap(_ sender: UIButton) {
+        if labelResult.text == "0" || (isSecond && secondOperand==0.0){
+            labelResult.text = ""
+        }
+        if let value = sender.titleLabel?.text {
+            labelResult.text = labelResult.text! + value
+        }
+        if isSecond {
+            secondOperand = (labelResult.text! as NSString).doubleValue
+        }else {
+            firstOperand = (labelResult.text! as NSString).doubleValue
+        }
+    }
+    
+    @IBAction func operatorTap(_ sender: UIButton) {
+        if firstOperand != 0 {
+            isSecond = true
+            decimalPointFlag = false
+            
+            switch sender.titleLabel!.text! {
+            case "+":
+                operatorFlag = "+"
+            case "-":
+                operatorFlag = "-"
+            case "×":
+                operatorFlag = "*"
+            case "÷":
+                operatorFlag = "/"
+            default:
+                operatorFlag = ""
+            }
+        }
+    }
+    
+    @IBAction func decimalPointTap(_ sender: Any) {
+        if !decimalPointFlag {
+            labelResult.text = labelResult.text!+"."
+            if isSecond {
+                secondOperand = ( labelResult.text! as NSString ).doubleValue
+            }else{
+                firstOperand = ( labelResult.text! as NSString ).doubleValue
+            }
+        }
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        labelResult.text = "0"
+        firstOperand = 0.0
+        secondOperand = 0.0
+        decimalPointFlag = false
+        isSecond = false
+        operatorFlag = ""
+    }
+    
+    @IBAction func resultTap(_ sender: Any) {
+        if isSecond {
+            if operatorFlag == "0" && secondOperand == 0 {
+                print("Error: 除数不能为0")
+                return
+            }
+            var result:Double = 0.0
+            switch operatorFlag {
+            case "+":
+                result = firstOperand+secondOperand
+            case "-":
+                result = firstOperand-secondOperand
+            case "*":
+                result = firstOperand*secondOperand
+            case "/":
+                result = firstOperand/secondOperand
+            default:
+                result=0.0
+            }
+            labelResult.text = result.description
+            print("第一操作数:\(firstOperand)")
+            print("操作符:\(operatorFlag)")
+            print("第二操作数:\(secondOperand)")
+            print("结果:\(result)")
+        }
+    }
+    
 }
 
